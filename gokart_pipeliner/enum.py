46a4df0a68fe7b809b724ad62e6b695584565725
vaaaaanquish <6syun9@gmail.com>
2021-01-12 22:00:23 +0900

    add return_value param

--- gokart_pipeliner/enum.py
+++ gokart_pipeliner/enum.py
@@ -1,7 +1,8 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Union, Any
 from enum import Enum
 
 
 class TYPING(Enum):
     PARAMS = Dict[str, Dict[str, Any]]
     STR_LIST = List[str]
+    RETURN_VALURE = Union[None, Any]
